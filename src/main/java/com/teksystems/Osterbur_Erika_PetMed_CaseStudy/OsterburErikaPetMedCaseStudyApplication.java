5b32e03daf4c60be7ff2c6925dab4215c2320c4d
erikaosterbur <erikaosterbur@gmail.com>
2022-04-02 12:52:36 -0500

    Initial file set up - pom file and application properties configuration

---
+++ src/main/java/com/teksystems/Osterbur_Erika_PetMed_CaseStudy/OsterburErikaPetMedCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.teksystems.Osterbur_Erika_PetMed_CaseStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsterburErikaPetMedCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsterburErikaPetMedCaseStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teksystems/Osterbur_Erika_PetMed_CaseStudy/OsterburErikaPetMedCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teksystems.Osterbur_Erika_PetMed_CaseStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsterburErikaPetMedCaseStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
